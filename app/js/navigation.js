0208c00be211ad3fb7d2722d4e85715a22cf8513
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-03-17 22:29:40 -0700

    Added structure for interval sidebar Structure was added to the sidebar that shows various timer intervals, including recommended intervals and custom intervals. On Google Chrome, the sidebar seems to work fine, but when starting the electron app, the application closes once the 'close' text is pressed; the sidebar is supposed to retract, showing the main screen.

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -2,9 +2,12 @@ function toggleSideNavigation(isOpen) {
     if(isOpen === 1) //Open the sidebar
     {
         document.getElementById("change-interval-sidebar").style.width="100%"
+        document.getElementById("change-interval-sidebar").style.left="0"
     }
     if(isOpen === 0) //Open the sidebar
     {
-        document.getElementById("change-interval-sidebar").style.width="0%"
+        document.getElementById("change-interval-sidebar").style.width="400px"
+        document.getElementById("change-interval-sidebar").style.left="-400px"
+
     }
 }
