66804ccffdd7248cc9467d7bce89f25e0e60c290
unknown <24364012+EanKeen@users.noreply.github.com>
2018-05-09 10:45:26 -0700

    Only one sidebar active at a time Only either the sidebar that changes the interval at which the clock runs or the sidebar that shows that statistics can be shown at the same time.

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,23 +1,31 @@
+var statisticsOpen = false;
+var customTimerOpen = false;
+
 function toggleCustomIntervalSidebar(isOpen) {
-  if(isOpen === 1)  //Open the sidebar
+  if(isOpen === 1 && statisticsOpen == false)  //Open the sidebar
   {
+    customTimerOpen = true;
     document.getElementById("change-interval-sidebar").style.left="0"
     document.getElementById("change-interval-sidebar").style.width="100%"
   }
   if(isOpen === 0)  //Close the sidebar
   {
+    customTimerOpen = false;
     document.getElementById("change-interval-sidebar").style.left="-100%"
   }
 }
 
+//Opens and closes the statistics sidebar
 function toggleStatisticsSidebar(isOpen) {
-  if(isOpen === 1)  //Open up the sidebar
+  if(isOpen === 1 && customTimerOpen == false)  //Open up the sidebar
   {
+    statisticsOpen = true;
     document.getElementById("get-statistics").style.left="0"
     document.getElementById("get-statistics").style.width="100%"
   }
   if(isOpen === 0)  //Close the sidebar
   {
+    statisticsOpen = false;
     document.getElementById("get-statistics").style.left="-100%"
   }
 }
