7963a1d10fb87c53bd423e082dcb4f6d242b6993
unknown <24364012+EanKeen@users.noreply.github.com>
2018-05-09 10:34:21 -0700

    Added statistics sidebar Added the statistics sidebar. It opens and closes properly, There are no graphs or data shown yet.

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,11 +1,23 @@
-function toggleSideNavigation(isOpen) {
-  if(isOpen === 1) //Open the sidebar
+function toggleCustomIntervalSidebar(isOpen) {
+  if(isOpen === 1)  //Open the sidebar
   {
     document.getElementById("change-interval-sidebar").style.left="0"
     document.getElementById("change-interval-sidebar").style.width="100%"
   }
-  if(isOpen === 0) //Close the sidebar
+  if(isOpen === 0)  //Close the sidebar
   {
     document.getElementById("change-interval-sidebar").style.left="-100%"
+  }
+}
+
+function toggleStatisticsSidebar(isOpen) {
+  if(isOpen === 1)  //Open up the sidebar
+  {
+    document.getElementById("get-statistics").style.left="0"
+    document.getElementById("get-statistics").style.width="100%"
+  }
+  if(isOpen === 0)  //Close the sidebar
+  {
+    document.getElementById("get-statistics").style.left="-100%"
   }
 }
