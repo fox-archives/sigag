8ac3921bbe1d013bac94f37c0e5387059ed1f627
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-02-02 11:46:26 -0800

    Use imported JavaScript modules

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+// main.js
+// Work on modularization of files relevant to timer here
+
+import { navigation } from "./navigation.js"
+navigation()
+console.log(navigation);
--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,36 +1,39 @@
-let statisticsOpen = false;
-let customTimerOpen = false;
+export function navigation() {
+  let statisticsOpen = false;
+  let customTimerOpen = false;
 
-// Function that either opens or closes customInterval sidebar, depending on parameter, and if the statistics sidebar is open
-function toggleCustomIntervalSidebar(isOpen) {
-  // Opens customInterval sidebar, but only if the statistics sidebar is not open
-  if(isOpen === 1 && statisticsOpen == false)
-  {
-    customTimerOpen = true;
-    document.getElementById("change-interval").style.left="0"
-    document.getElementById("change-interval").style.width="100%"
+  // Function that either opens or closes customInterval sidebar, depending on parameter, and if the statistics sidebar is open
+  function toggleCustomIntervalSidebar(isOpen) {
+    // Opens customInterval sidebar, but only if the statistics sidebar is not open
+    if(isOpen === 1 && statisticsOpen == false)
+    {
+      customTimerOpen = true;
+      document.getElementById("change-interval").style.left="0"
+      document.getElementById("change-interval").style.width="100%"
+    }
+    // Closes the customInterval sidebar
+    if(isOpen === 0)
+    {
+      customTimerOpen = false;
+      document.getElementById("change-interval").style.left="-100%"
+    }
   }
-  // Closes the customInterval sidebar
-  if(isOpen === 0)
-  {
-    customTimerOpen = false;
-    document.getElementById("change-interval").style.left="-100%"
+
+  // Function that either opens or closes statistics sidebar, depending on parameter, and if the customInterval sidebar is open
+  function toggleStatisticsSidebar(isOpen) {
+    // Opens up statistics sidebar, but only if the intervalSidebar is not open
+    if(isOpen === 1 && customTimerOpen == false)
+    {
+      statisticsOpen = true;
+      document.getElementById("get-statistics").style.left="0"
+      document.getElementById("get-statistics").style.width="100%"
+    }
+    // Closes the statisticsSidebar
+    if(isOpen === 0)
+    {
+      statisticsOpen = false;
+      document.getElementById("get-statistics").style.left="-100%"
+    }
   }
 }
 
-// Function that either opens or closes statistics sidebar, depending on parameter, and if the customInterval sidebar is open
-function toggleStatisticsSidebar(isOpen) {
-  // Opens up statistics sidebar, but only if the intervalSidebar is not open
-  if(isOpen === 1 && customTimerOpen == false)
-  {
-    statisticsOpen = true;
-    document.getElementById("get-statistics").style.left="0"
-    document.getElementById("get-statistics").style.width="100%"
-  }
-  // Closes the statisticsSidebar
-  if(isOpen === 0)
-  {
-    statisticsOpen = false;
-    document.getElementById("get-statistics").style.left="-100%"
-  }
-}
